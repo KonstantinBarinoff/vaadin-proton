601dcbb6b69c9d989a4e1d3448e9d09400aa499d
Sergey Ovsyannikov <ovsyannikov_sn@energomash.ru>
2021-08-04 14:51:14 +0300

    Исправил слетевшие отступы

--- sumo/src/main/java/sumo/entities/BzemDepartment.java
+++ sumo/src/main/java/sumo/entities/BzemDepartment.java
@@ -12,49 +12,48 @@ public class BzemDepartment {
     private String departmentName;
     private BzemDepartment parent;
 
-    
-    
+
     @Override
     public int hashCode() {
-	final int prime = 31;
-	int result = 1;
-	result = prime * result + ((departmentName == null) ? 0 : departmentName.hashCode());
-	result = prime * result + ((departmentNumber == null) ? 0 : departmentNumber.hashCode());
-	result = prime * result + ((departmentParentNumber == null) ? 0 : departmentParentNumber.hashCode());
-	return result;
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((departmentName == null) ? 0 : departmentName.hashCode());
+        result = prime * result + ((departmentNumber == null) ? 0 : departmentNumber.hashCode());
+        result = prime * result + ((departmentParentNumber == null) ? 0 : departmentParentNumber.hashCode());
+        return result;
     }
 
     @Override
     public boolean equals(Object obj) {
-	if (this == obj)
-	    return true;
-	if (obj == null)
-	    return false;
-	if (getClass() != obj.getClass())
-	    return false;
-	BzemDepartment other = (BzemDepartment) obj;
-	if (departmentName == null) {
-	    if (other.departmentName != null)
-		return false;
-	} else if (!departmentName.equals(other.departmentName))
-	    return false;
-	if (departmentNumber == null) {
-	    if (other.departmentNumber != null)
-		return false;
-	} else if (!departmentNumber.equals(other.departmentNumber))
-	    return false;
-	if (departmentParentNumber == null) {
-	    if (other.departmentParentNumber != null)
-		return false;
-	} else if (!departmentParentNumber.equals(other.departmentParentNumber))
-	    return false;
-	return true;
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        BzemDepartment other = (BzemDepartment) obj;
+        if (departmentName == null) {
+            if (other.departmentName != null)
+                return false;
+        } else if (!departmentName.equals(other.departmentName))
+            return false;
+        if (departmentNumber == null) {
+            if (other.departmentNumber != null)
+                return false;
+        } else if (!departmentNumber.equals(other.departmentNumber))
+            return false;
+        if (departmentParentNumber == null) {
+            if (other.departmentParentNumber != null)
+                return false;
+        } else if (!departmentParentNumber.equals(other.departmentParentNumber))
+            return false;
+        return true;
     }
 
     @Override
     public String toString() {
-	return "BzemDepartment [departmentNumber=" + departmentNumber + ", departmentParentNumber="
-		+ departmentParentNumber + ", departmentName=" + departmentName + "]";
+        return "BzemDepartment [departmentNumber=" + departmentNumber + ", departmentParentNumber="
+                + departmentParentNumber + ", departmentName=" + departmentName + "]";
     }
 
 }
